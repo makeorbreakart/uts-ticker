94ecfcdea8437a5e7926c6de0ba99e84dfa5993e
makeorbreakart <makeorbreakart@gmail.com>
2022-03-26 19:31:49 +1100

    Updates

--- bundle.js
+++ bundle.js
@@ -174,11 +174,8 @@ __webpack_require__.r(__webpack_exports__);
 
 
 function NewsTicker() {
-  var fullTextWidth = (0,_imagemarker_use_text_width__WEBPACK_IMPORTED_MODULE_4__.useTextWidth)({
-    text: _headlines__WEBPACK_IMPORTED_MODULE_5__["default"],
-    font: '3em monospace'
-  }); // const fullTextWidth = 9845793475;
-  // this is async it seems - will be easiest to calculate value of this once when final text file is ready
+  // const fullTextWidth = useTextWidth({text: headlinesText, font: '3em monospace'});
+  var fullTextWidth = 18525.12890625; // this is async it seems - will be easiest to calculate value of this once when final text file is ready
   // then include as hardcoded var as shown aove
   // this calculation gets offset as a per-character scaled number
 
@@ -186,8 +183,9 @@ function NewsTicker() {
   var charWidth = fullTextWidth / charCount;
   var _speed = 8; // will need to set this as literal time when ticker goes live
   // currently is set to have started 30 seconds ago
+  // const startTime = Date.now();
 
-  var startTime = Date.now();
+  var startTime = 1648283129044;
   var timeStamp = Date.now();
 
   var _offset = _speed * (timeStamp - startTime) * charWidth * -1;
@@ -197,7 +195,7 @@ function NewsTicker() {
   console.log(_offset);
   return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(react_ticker__WEBPACK_IMPORTED_MODULE_1__["default"], {
     speed: _speed,
-    offset: 0
+    offset: _offset
   }, function () {
     return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(react__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
       className: "mainTicker"
